2905905245f032502900a94fcb56861a1e81b4f1
Anu Thomas Chandy <anuamd@hotmail.com>
2018-11-02 16:37:44 -0700

    Update install_apache.sh

--- azure-samples/src/main/resources/install_apache.sh
+++ azure-samples/src/main/resources/install_apache.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-sudo echo "deb http://archive.ubuntu.com/ubuntu trusty main universe restricted multiverse" > /etc/apt/sources.list
-
 sudo apt-get update
-
-sudo apt-get dist-upgrade
 
 # install apache
 sudo apt-get -y install apache2
